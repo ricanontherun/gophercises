a7149b03cc6542a51ca7bad14233d4da8aae7d3e
romanc <croman@philly.com>
2018-04-12 08:39:52 -0400

    Finished basics of quiz program

--- quiz/main.go
+++ quiz/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type quiz struct {
+	questions []question
+	grade     float32
+}
+
+type question struct {
+	text            string
+	correctAnswer   string
+	attemptedAnswer string
+}
 
 func checkError(e error) {
 	if e != nil {
@@ -22,9 +35,34 @@ func main() {
 	}
 
 	records, err := reader.ReadAll()
-
 	checkError(err)
 
-	fmt.Println(records)
-	fmt.Println("Hi, this is a go program")
+	// Prep our quiz data structure.
+	quiz := quiz{
+		questions: make([]question, len(records)),
+	}
+
+	for i, line := range records {
+		quiz.questions[i] = question{text: line[0], correctAnswer: strings.TrimSpace(line[1])}
+	}
+
+	lineReader := bufio.NewReader(os.Stdin)
+	answeredCorrectly := 0
+
+	// Now, spin through the quesitons we have, and prompt the user.
+	for i, question := range quiz.questions {
+		fmt.Printf("%d) %s: ", i+1, question.text)
+
+		// Prompt for their answer.
+		answer, err := lineReader.ReadString('\n')
+		checkError(err)
+
+		question.attemptedAnswer = strings.TrimSpace(answer)
+
+		if question.attemptedAnswer == question.correctAnswer {
+			answeredCorrectly++
+		}
+	}
+
+	fmt.Printf("You answered %d out of %d questions correctly\n", answeredCorrectly, len(quiz.questions))
 }
